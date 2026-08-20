0c41e882a7675c49b6306fd493126dbdeeece433
Victor Alves <santos.alves.victor@escola.pr.gov.br>
2026-08-20 12:42:35 -0300

    mnn

--- main.js
+++ main.js
@@ -1,2 +1,14 @@
 const numerosenha = document.queryselector ('.parametro-senha__texto')
 numeroSenha.textContent = 5;
+
+tmanhoSenha = 8
+
+const botoes = document.queryselector ('.parametro-senha__texto')
+queryselectorAll('parametro-ssenha__botao')
+
+botoes[0].onclick = diminuir;
+
+function diminuir (){
+tamanhoSenha--;
+numeroSenha.textContent = tamanhoSenha;
+}
